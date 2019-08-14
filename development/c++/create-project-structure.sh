c176d3348ee289ab38548bbeac858a28cdd649c0
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-08-14 18:57:41 +0200

    Add comments

--- development/c++/create-project-structure.sh
+++ development/c++/create-project-structure.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# Create all directories.
 mkdir -p bin build doc include lib src test
+# Create all files.
 touch README.md LICENSE .gitignore
+# Populate .gitignore in order to ignore build and lib directories.
 echo '# Ignore the build and lib dirs
 build
 lib/*
