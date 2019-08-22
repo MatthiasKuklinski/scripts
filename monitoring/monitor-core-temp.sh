7d163bcece2a1f8cff2d4b9526ecbb2a4bd42a9b
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-08-22 22:49:41 +0200

    Print timestamp

--- monitoring/monitor-core-temp.sh
+++ monitoring/monitor-core-temp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 while true; do
     temp=$(vcgencmd measure_temp | grep -o "[0-9]*\.[0-9]*'C")
-    printf "%s\n" "$temp"
+    timestamp=$(date +"%Y-%m-%d %T")
+    printf "%s%7s\n" $(date +"%Y-%m-%d %T") "$temp"
     sleep 1
 done
