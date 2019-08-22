fad29c29fbd5a64ef679b846f3aac75e514245f8
Matthias Kuklinski <contact@matthiaskuklinski.com>
2019-08-22 22:50:51 +0200

    Output correct timestamp variable

--- monitoring/monitor-core-temp.sh
+++ monitoring/monitor-core-temp.sh
@@ -3,6 +3,6 @@
 while true; do
     temp=$(vcgencmd measure_temp | grep -o "[0-9]*\.[0-9]*'C")
     timestamp=$(date +"%Y-%m-%d %T")
-    printf "%s%7s\n" $(date +"%Y-%m-%d %T") "$temp"
+    printf "%s%7s\n" "$timestamp" "$temp"
     sleep 1
 done
