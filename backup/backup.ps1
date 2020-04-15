dbecc6100fa953c7fe8ceb749134eb463d1d58d0
Matthias Kuklinski <contact@matthiaskuklinski.com>
2020-04-15 12:28:46 +0200

    Add sleep comment

--- backup/backup.ps1
+++ backup/backup.ps1
@@ -7,5 +7,5 @@ $cfg = Get-Content -Raw -Path $cfgPath | ConvertFrom-Json
 forEach ($e in $cfg) {
     $target = [IO.Path]::Combine($e.target, $(Get-Date -f dd.MM.yyyy_HH-mm-ss) + ".zip")
     & 7z a -r -tzip $target $($e.src)
-    start-sleep -s 3
+    start-sleep -s 3    # Avoid compressing into the wrong archive(from the previous compression).
 }
